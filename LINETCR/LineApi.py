bed8cfb0d5425e598786511e93c00f140d7ca209
PRANKBOTS <33993971+Aprank@users.noreply.github.com>
2018-01-28 00:16:05 +0700

    Update LineApi.py

--- LINETCR/LineApi.py
+++ LINETCR/LineApi.py
@@ -37,7 +37,7 @@ class LINE:
     self.authToken = self.Talk.authToken
     self.cert = self.Talk.cert
     self._headers = {
-              'X-Line-Application': 'DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0', 
+              'X-Line-Application': 'IOSIPAD\t7.18.0\tiPhone OS\t11.12.1', 
               'X-Line-Access': self.authToken, 
               'User-Agent': 'Line/6.0.0 iPad4,1 9.0.2'
    }
@@ -350,7 +350,7 @@ class LINE:
 
       prof = self.getProfile()
 
-      print("MikanBOT")
+      print("")
       print("mid -> " + prof.mid)
       print("name -> " + prof.displayName)
       print("authToken -> " + self.authToken)
